8c8ff64e0005549d9a34d3fe76773dfe899f12d0
Hasan Haghniya <secretkeeper@streamware.co>
2024-05-17 00:39:24 +0330

    init HashMap implementation

--- src/fundamentals/option/src/main.rs
+++ src/fundamentals/option/src/main.rs
@@ -1,16 +1,3 @@
-// Topic: Option
-//
-// Requirements:
-// * Print out the details of a student's locker assignment
-// * Lockers use numbers and are optional for students
-//
-// Notes:
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
-
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
-
 struct Student {
     name: String,
     locker: Option<i32>,
